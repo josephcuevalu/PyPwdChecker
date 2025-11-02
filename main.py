f03edecd1ebf82d1bdcfc261484b54db19dae254
joey727 <joseph.cueva.727@gmail.com>
2025-11-01 22:19:25 -0400

    feat: inital password checker

---
+++ main.py
@@ -0,0 +1,25 @@
+import string
+import getpass
+
+def check_pwd():
+    password = getpass.getpass("Enter Password: ")
+    strength = 0
+    remarks = ''
+    lower_count = upper_count = num_count = wspace_count = special_count = 0
+
+    for char in list(password):
+        if char in string.ascii_lowercase:
+            lower_count += 1
+
+        elif char in string.ascii_uppercase:
+            upper_count += 1
+        elif char in string.digits:
+            num_count += 1
+        elif char == ' ':
+            wspace_count += 1
+        else:
+            special_count += 1
+
+    if lower_count >= 1:
+        strength += 1
+
